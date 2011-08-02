13ad7ba3858d430d66fd013266deacc1e60d3404
Peter Mack Grubb <gimlithepirate@gmail.com>
2011-08-02 14:06:01 -0500

    Updates setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'python-github3',
-    version = '0.0.2',
+    version = '0.0.3',
     description = "An API interface that uses the new github v3 api",
     packages = ['github3',],
 )
